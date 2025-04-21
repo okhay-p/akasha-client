a570149705c9cdc6d8c1b98e29c58f2220331cca
okhay-p <oakkarphyo7@gmail.com>
2025-04-21 15:49:17 +0800

    Test google login

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,11 @@ import { useAuth } from "@/contexts/AuthContext";
 import { Link } from "react-router-dom";
 
 function Header() {
-  const { isAuthenticated, login, logout } = useAuth();
+  const { isAuthenticated, logout } = useAuth();
+
+  const handleGoogleOAuthLogin = () => {
+    window.location.href = "http://localhost:8080/auth/google";
+  };
 
   return (
     <div className="max-w-3xl mx-auto bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 md:top-2 flex justify-between items-center p-2 border-b md:border md:rounded-4xl shadow-md md:shadow-xl shadow-primary/20 z-10 font-custom-sans">
@@ -37,7 +41,7 @@ function Header() {
           <Button
             variant="outline"
             className="text-xs w-[80px]"
-            onClick={() => login()}
+            onClick={handleGoogleOAuthLogin}
           >
             Sign In
           </Button>
