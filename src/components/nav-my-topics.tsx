752574ea64dc62a560f9a8bf8811b9394b915e89
okhay-p <oakkarphyo7@gmail.com>
2025-04-30 11:14:44 +0800

    Fix sidebar null error

--- src/components/nav-my-topics.tsx
+++ src/components/nav-my-topics.tsx
@@ -45,63 +45,64 @@ export function NavMyTopics({
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarGroupLabel>My Topics</SidebarGroupLabel>
       <SidebarMenu>
-        {topics.map((item) => (
-          <SidebarMenuItem key={item.title}>
-            <NavLink to={"/topic/" + item.id}>
-              {({ isActive }) => (
-                <SidebarMenuButton
-                  isActive={isActive}
-                  className="hover:cursor-pointer"
-                >
-                  <span>{item.emoji}</span>
-                  <span>{item.title}</span>
-                </SidebarMenuButton>
-              )}
-            </NavLink>
-            <AlertDialog>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuAction showOnHover>
-                    <MoreHorizontal />
-                    <span className="sr-only">More</span>
-                  </SidebarMenuAction>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  className="w-56 rounded-lg"
-                  side={isMobile ? "bottom" : "right"}
-                  align={isMobile ? "end" : "start"}
-                >
-                  <AlertDialogTrigger>
-                    <DropdownMenuItem>
-                      <CircleMinus className="text-muted-foreground" />
-                      <span>Remove from My Topics</span>
-                    </DropdownMenuItem>
-                  </AlertDialogTrigger>
-                  {/* <DropdownMenuItem>
+        {topics &&
+          topics.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <NavLink to={"/topic/" + item.id}>
+                {({ isActive }) => (
+                  <SidebarMenuButton
+                    isActive={isActive}
+                    className="hover:cursor-pointer"
+                  >
+                    <span>{item.emoji}</span>
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                )}
+              </NavLink>
+              <AlertDialog>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuAction showOnHover>
+                      <MoreHorizontal />
+                      <span className="sr-only">More</span>
+                    </SidebarMenuAction>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    className="w-56 rounded-lg"
+                    side={isMobile ? "bottom" : "right"}
+                    align={isMobile ? "end" : "start"}
+                  >
+                    <AlertDialogTrigger>
+                      <DropdownMenuItem>
+                        <CircleMinus className="text-muted-foreground" />
+                        <span>Remove from My Topics</span>
+                      </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                    {/* <DropdownMenuItem>
                   <Link className="text-muted-foreground" />
                   <span>Copy Link</span>
                 </DropdownMenuItem> */}
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Yor progress will be reset for this topic.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction
-                    onClick={() => deleteProgress(item.id)}
-                  >
-                    Remove
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </SidebarMenuItem>
-        ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Yor progress will be reset for this topic.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => deleteProgress(item.id)}
+                    >
+                      Remove
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </SidebarMenuItem>
+          ))}
       </SidebarMenu>
     </SidebarGroup>
   );
